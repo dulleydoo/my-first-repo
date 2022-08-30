36406e1abd83edd233a7eac32e8ac844ffb31c6e
dulleydoo <karem.jabar2015@gmail.com>
2022-08-30 19:29:30 -0400

    more work

--- Person/src/main/java/Person.java
+++ Person/src/main/java/Person.java
@@ -40,10 +40,10 @@ public class Person {
         myNum3 = 35;
         System.out.println(myNum3);
 
-//        final int myNum4 = 33;
-//        myNum4 = 38;
-//        System.out.println(myNum4);
-//        The result would be 33. By using "final", it is unchangable
+        //final int myNum4 = 33;
+        //myNum4 = 38;
+        //System.out.println(myNum4);
+        //The result would be 33. By using "final", it is unchangable
 
         int Num = 22;
         float myFloatNum = 33.5f;
@@ -78,6 +78,49 @@ public class Person {
         int g, h, i;
         g = h = i = 50;
         System.out.println(g + h + i);
+
+
+//Identifiers
+        //good
+        int minsPerHour = 60;
+        //bad
+        int m = 60;
+
+
+//Data Types
+        // Integer (whole number)
+        int Numb = 20;
+        // Floating point number
+        float FloatNum = 10.99f;
+        // Character
+        char Letter = 'K';
+        // Boolean
+        boolean Bool = true;
+        // String
+        String Text = "Hello World";
+
+
+//NUMBERS
+        int number = 10000;
+        float floatNum = 10.32f;
+        double doubleNum = 10.32d;
+
+
+//BOOLEAN
+        boolean iPlaySoccer = true;
+        boolean imSevenFoot = false;
+        System.out.println(iPlaySoccer);
+        System.out.println(imSevenFoot);
+
+
+
+//Characters
+        char firstLetterofName = 'K';
+        char myGrade = 'A';
+
+
+
+
 
 
 
